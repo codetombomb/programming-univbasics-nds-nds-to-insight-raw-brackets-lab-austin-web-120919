e9f428bafc4a33d655caa1c2a68300870c1824dc
Tomas Tobar <codetombomb@gmail.com>
2019-12-09 02:17:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,58 +25,20 @@ require "pry"
 
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-
-
   row_index = 0
-  # container = 0
 director_totals = {}
-  # director_totals = {director_name => container}
-
-      # binding.pry
-        #9 directors
     while row_index < directors_database.length do
       director_name = directors_database[row_index][:name]
-      # binding.pry
         container = 0
-
       element_index = 0
-    # directors_database[row_index][:movies][element_index][:worldwide_gross]
-      # binding.pry
-      #                                    movie list length (6)
       while element_index < directors_database[row_index][:movies].length do
-        # break if element_index == 2
         container += directors_database[row_index][:movies][element_index][:worldwide_gross]
-      # binding.pry
       element_index += 1
-      # binding.pry
       end
-      # binding.pry
       director_totals[director_name] = container
-      #binding.pry
         row_index += 1
-        # binding.pry
-
     end
-    # binding.pry
-
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  # result = {
-  # }
-
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
- # Be sure to return the result at the end!
  director_totals
- # binding.pry
 end
 
 # binding.pry
